de065c1e4102b8bba4ccdf3951d855de2ab033c6
achrefbenammar404 <achref.benammar404@gmail.com>
2025-02-02 18:09:37 +0100

    feat: bump version to 1.0.1 in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Aiklyra',
-    version='1.0.0',
+    version='1.0.1',
     author='Aiklyra',
     author_email='achref.benammar@ieee.org',
     description='A Python client library for Aiklyra API',
